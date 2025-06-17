021ccaa992a55ce5c74a84668a4762f78e8a3ffe
marcos <martinmarcospablo@gmail.com>
2025-06-17 19:04:28 -0300

    Primer práctica selenium

---
+++ SeleniumLoginTest/UnitTest1.cs
@@ -0,0 +1,45 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace SeleniumLoginTest;
+
+public class Tests
+{
+    private IWebDriver driver;
+    [SetUp]
+    public void Setup()
+    {
+        driver = new ChromeDriver();
+
+    }
+
+    [Test]
+    public void TestLogin()
+    {
+        driver.Navigate().GoToUrl("file:///C:/Users/cotom/OneDrive/Escritorio/SeleniumLogin/SeleniumLoginTest/login.html");
+
+        IWebElement usernameField = driver.FindElement(By.Id("username"));
+        IWebElement passwordField = driver.FindElement(By.Id("password"));
+        IWebElement loginButton = driver.FindElement(By.Id("loginButton"));
+
+        usernameField.SendKeys("testuser");
+        passwordField.SendKeys("testpassword");// se puede reemplazar por un valor real
+        loginButton.Click();
+
+        System.Threading.Thread.Sleep(2000); // Espera para ver el resultado
+
+        Assert.That(driver.Url, Is.EqualTo("file:///C:/Users/cotom/OneDrive/Escritorio/SeleniumLogin/SeleniumLoginTest/success.html"));
+        Assert.That(driver.PageSource.Contains("Formulario completado exitosamente"), Is.True);
+    }
+    [TearDown]
+    public void Teardown()
+    {
+        if(driver != null)
+        {
+            driver.Quit();
+            driver.Dispose();
+        }   
+        
+    }
+}
